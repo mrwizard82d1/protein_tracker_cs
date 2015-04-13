08b2b4c94e3c7eabc3475562976930ff53d25597
Lawrence Allan Jones <lawrence.jones@openlink.com>
2015-04-13 15:49:44 -0500

    Write CanIAddAndSubtractProteins class.
    I thought it would work - but it failed. (I think I forgot to make the
    class **public**.)

---
+++ ProteinTracker/ProteinTrackerDotNetSlim/CanIAddAndSubtractProteins.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ProteinTrackerDotNetSlim
+{
+    class CanIAddAndSubtractProteins
+    {
+        private TrackerService _sut = new TrackerService();
+
+        public int Amount { get; set; }
+
+        public String Operation { get; set; }
+
+        // Will the .NET slim runner search for and invoke an `Execute` 
+        // method?
+        public void Execute()
+        {
+            if (Operation.Equals("add"))
+            {
+                _sut.AddProtein(Amount);
+            } else if (Operation.Equals("subtract"))
+            {
+                _sut.RemoveProtein(Amount);
+            }
+        }
+
+        public int TotalIs()
+        {
+            return _sut.Total;
+        }
+
+    }
+
+    internal class TrackerService
+    {
+        public int Total { get; private set; }
+
+        public void AddProtein(int amount)
+        {
+            
+        }
+
+        public void RemoveProtein(int amount)
+        {
+            
+        }
+    }
+}
