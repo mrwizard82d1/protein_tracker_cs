b4c3de9deab16187a1840503a1d719e96c1338d5
Lawrence Allan Jones <lawrence.jones@openlink.com>
2015-04-13 12:20:45 -0500

    And now the tests pass.
    Note that I did not even need to add the Slim library since I did not
    actually use anything in the library for my test. The Slim library is
    perfectly capable of using a POCO (Plain ol' C# object).

--- ProteinTracker/ProteinTrackerDotNetSlim/HelloSlim.cs
+++ ProteinTracker/ProteinTrackerDotNetSlim/HelloSlim.cs
@@ -2,5 +2,12 @@
 {
     public class HelloSlim
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName()
+        {
+            return FirstName + " " + LastName;
+        }
     }
 }
