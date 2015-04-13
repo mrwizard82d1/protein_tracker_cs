cf94545bd48796347fba0ac6d0e02ef48917413d
Lawrence Allan Jones <lawrence.jones@openlink.com>
2015-04-13 10:47:21 -0500

    It's working!
    Added `FullName()` method. Everything is green!

--- ProteinTracker/ProteinTrackerDotNetFit/HelloWorld.cs
+++ ProteinTracker/ProteinTrackerDotNetFit/HelloWorld.cs
@@ -6,5 +6,10 @@ namespace ProteinTrackerDotNetFit
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public string FullName()
+        {
+            return FirstName + " " + LastName;
+        }
     }
 }
